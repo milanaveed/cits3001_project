5cea9e316bc2a8a64e1679a2390f64094a359ab2
oddnaveed <84265464+oddnaveed@users.noreply.github.com>
2023-10-04 16:47:14 +0800

    Add

--- reinforcement-learning/Installation Commands.md
+++ reinforcement-learning/Installation Commands.md
@@ -1,17 +1,23 @@
+# Gym
+pip install gym==v0.21.0
+- stable-baselines3 doesn't support gym v0.26
+
+# Super Mario Bros
+pip install gym_super_mario_bros==v7.3.0
 
 # PyTorch
+For compatibility with gym version and stable-baselines3, choose the previous versions around v1.10.1
 https://pytorch.org/get-started/previous-versions/
 
 ## For MacOS
 conda install pytorch::pytorch torchvision torchaudio -c pytorch
 
 ## For Windows
-### CUDA 11.3
-- pip3 install torch torchvision torchaudio --index-url 
-- conda install pytorch==1.10.1 torchvision==0.11.2 torchaudio==0.10.1 cudatoolkit=11.3 -c pytorch -c conda-forg
+### CUDA
+- pip install torch==1.10.1+cu113 torchvision==0.11.2+cu113 torchaudio===0.10.1+cu113 -f
 
 # Stable Baselines
-pip install stable-baselines3[extra]
+pip install stable-baselines3==1.16.0
 
-- stable-baselines3 v2 doesn't support gym 
-- stable-baselines3 v1.x doesn't support gym v0.26
+- stable-baselines3 V2 doesn't support gym 
+- stable-baselines3 V1.x doesn't support gym v0.26
--- reinforcement-learning/reinforcementLearning.py
+++ reinforcement-learning/reinforcementLearning.py
@@ -1,13 +1,18 @@
 # #import the game
 import gym
 import gym_super_mario_bros
+import os
 # #import the Joypad wrapper
 from nes_py.wrappers import JoypadSpace
 # #import the simplified controls
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 from gym.wrappers import GrayScaleObservation
 from stable_baselines3.common.vec_env import VecFrameStack, DummyVecEnv
+from stable_baselines3 import PPO
+from stable_baselines3.common.callbacks import BaseCallback
+# from wrappers import SkipFrame, ResizeObservation
 from matplotlib import pyplot as plt
+
 
 # Beginner Tutorial: https://www.youtube.com/watch?v=2eeYqJ0uBKE
 # Level Up Tutorial: https://www.youtube.com/watch?v=PxoG0A2QoFs
@@ -19,12 +24,19 @@ from matplotlib import pyplot as plt
 
 # Set up game environment and simplify actions
 env = gym_super_mario_bros.make("SuperMarioBros-v0")
-MY_MOVEMENT = SIMPLE_MOVEMENT[1:3] # sample the ['right'] and ['right', 'A'] actions
+# sample the ['right'] and ['right', 'A'] actions
+MY_MOVEMENT = SIMPLE_MOVEMENT[1:3]
 env = JoypadSpace(env, MY_MOVEMENT)
 # env = JoypadSpace(env, SIMPLE_MOVEMENT)
 
+# Skip frames
+# env = SkipFrame(env, skip=4)
+
 # Grayscale environment
 env = GrayScaleObservation(env, keep_dim=True)
+
+# Resize observation
+# env = ResizeObservation(env, shape=84)
 
 # Create a vectorized wrapper for an environment
 env = DummyVecEnv([lambda: env])
@@ -32,8 +44,8 @@ env = DummyVecEnv([lambda: env])
 # Stack the frames
 env = VecFrameStack(env, 4, channels_order='last')
 
-#todo: can we resize the frame?
-#todo: can we skip some frames?
+# todo: can we resize the frame? see what it'll look like after resize
+# todo: can we skip some frames?
 
 '''
      # 1. CREATE THE MARIO ENV
@@ -56,11 +68,55 @@ env = VecFrameStack(env, 4, channels_order='last')
 mario_env  = add_wrapper_functionality()
 '''
 
+# Train and save the training process
+class TrainAndSaveCallback(BaseCallback):
+
+    def __init__(self, check_freq, save_path, verbose=1):
+        super(TrainAndSaveCallback, self).__init__(verbose)
+        self.check_freq = check_freq
+        self.save_path = save_path
+
+    def _init_callback(self):
+        if self.save_path is not None:
+            os.makedirs(self.save_path, exist_ok=True)
+
+    def _on_step(self):
+        if self.n_calls % self.check_freq == 0:
+            model_path = os.path.join(
+                self.save_path, 'best_model_{}'.format(self.n_calls))
+            self.model.save(model_path)
+
+        return True
+
+
+DATA_DIR = './reinforcement-learning/train/'
+LOG_DIR = './reinforcement-learning/logs/'
+
+callback = TrainAndSaveCallback(check_freq=50000, save_path=DATA_DIR)
+model = PPO('CnnPolicy', env, verbose=1, tensorboard_log=LOG_DIR,
+            learning_rate=0.000005, n_steps=512)
+model.learn(4000000, callback=callback)
+
+model.save('test-model')
+
+# 1am 开始train
+
+#TODO: upgrade stablebaselines3 version to v1.18.0
+
+# Load the training result
+# model = PPO.load('./reinforcement-learning/train/best_model_40000')
+# state = env.reset()
+# while True:
+#     action, _ = model.predict(state)
+#     print(MY_MOVEMENT[action[0]])
+#     state, reward, done, info = env.step(action)
+#     env.render()
+
 # Testing the game
-state = env.reset()
-state, reward, done, info = env.step([1])
-state, reward, done, info = env.step([1])
-state, reward, done, info = env.step([1])
+# state = env.reset()
+# state, reward, done, info = env.step([1])
+# state, reward, done, info = env.step([1])
+# state, reward, done, info = env.step([1])
 # plt.figure(figsize=(15,12))
 # for idx in range(state.shape[3]):
 #     plt.subplot(1,4,idx+1)
@@ -78,9 +134,9 @@ state, reward, done, info = env.step([1])
 # Create a flag - restart or not
 # done = True
 # # Loop through each frame in the game
-# for step in range(100000): 
-#     # Start the game to begin with 
-#     if done: 
+# for step in range(100000):
+#     # Start the game to begin with
+#     if done:
 #         # Start the gamee
 #         env.reset()
 #     # Do random actions
